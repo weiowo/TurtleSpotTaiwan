035cb28e586035092f3a442aa33f3a5a27492dfc
weiwei <wutamy77@gmail.com>
2025-03-20 23:36:18 +0800

    create carousel components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import TurtleProfile from '@/components/TurtleProfile';
 import Banner from '@/components/Banner';
+import Carousel from '@/components/Carousel';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Banner />
       <TurtleProfile />
+      <Carousel />
     </div>
   );
 }
