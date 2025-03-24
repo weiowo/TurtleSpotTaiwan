4abd42320fd95493334d3ac91da6ad6d7f22245b
weiwei <wutamy77@gmail.com>
2025-03-24 21:53:49 +0800

    delete unecessary css and fix import issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Banner from '@/components/Banner';
 import Carousel from '@/components/Carousel';
 import Location from '@/components/Location';
 import InstagramStoryUI from '@/components/IGStories';
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
